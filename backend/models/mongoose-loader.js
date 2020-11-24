bad7a2af5aa5eda1c1734e3f29b85b44ba770c84
gimletus <gimlet.koban@gmail.com>
2020-11-24 16:40:46 +0900

    db connect and insert members

--- backend/models/mongoose-loader.js
+++ backend/models/mongoose-loader.js
@@ -5,6 +5,7 @@ const env = require('../.env');
 // mongodb://host:port/database
 const conMongo = mongoose.connect(`mongodb://${env.DBHOST}:${env.DBPORT}/${env.DBNAME}`, { useNewUrlParser: true });
 
+
 module.exports = {
   mongoose: mongoose,
   conMongo: conMongo
--- backend/routes/test.js
+++ backend/routes/test.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-var member = require('../models/t01_users');
+var con = require('../models/mongoose-loader');
+var memberModel = require('../models/t01_users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,14 +12,28 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
   // mongoに書き込み
   // member.
-  var schema = new member();
-  schema.name = req.body.name;
-  schema.email = req.body.email;
-  schema.tel = req.body.tel;
-  schema.password = req.body.password;
+  const db = con.mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
+    // we're connected!
+    console.log('DB接続中... You can cancel from ctrl + c');
+  });
+
+  const member = new memberModel.Member({
+    name: req.body.name,
+    email: req.body.email,
+    tel: req.body.tel,
+    password: req.body.password
+  });
   
-  console.log(req.body.name);
-  schema.save();
+  console.log('req.body.name : ' + req.body.name);
+  console.log('member.name : ' + member.name);
+
+  member.save(function (err, member) {
+    if (err) return console.error(err);
+    // member.speak();
+  });
+
   res.render('test', { title: 'Express'});
 });
 
