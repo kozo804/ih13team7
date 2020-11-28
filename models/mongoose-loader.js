c0d27e01fbac6e9c132e200b9a4113fad93451a3
kozo804 <fazsegtwin@gmail.com>
2020-11-28 22:34:04 +0900

    知らんけど設定追加した方がいいらしい

--- models/mongoose-loader.js
+++ models/mongoose-loader.js
@@ -3,7 +3,13 @@ const mongoose = require('mongoose');
 const env = require('../.env');
 
 // mongodb://host:port/database
-const conMongo = mongoose.connect(`mongodb://${env.DBHOST}:${env.DBPORT}/${env.DBNAME}`, { useNewUrlParser: true });
+const conMongo = mongoose.connect(
+  `mongodb://${env.DBHOST}:${env.DBPORT}/${env.DBNAME}`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 
 module.exports = {
