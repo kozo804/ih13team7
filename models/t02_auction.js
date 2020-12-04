81ef4a3201a6788d11fc69d4ab1554a5796a8a4b
gimletus <gimlet.koban@gmail.com>
2020-12-04 12:28:05 +0900

    edit db auction

--- models/t02_auction.js
+++ models/t02_auction.js
@@ -6,12 +6,13 @@ const mongoose = loader.mongoose;
 var Schema = mongoose.Schema;
 
 var auctionSchema = new Schema({
-	_id: Number,
+	// _id: Number,
 	auction_name: String,
 	start_time: Date,
 	end_time: Date,
 	rep_id: Number,
-	car_count: Number
+	car_count: Number,
+	car_ids : Object
 });
 
 module.Auction = mongoose.model('Auction', auctionSchema);
