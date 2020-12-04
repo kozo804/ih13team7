cfc55f848d8aa385544c14c4811cd2ae927b02eb
kozo804 <fazsegtwin@gmail.com>
2020-12-04 22:03:49 +0900

    修正、追加

--- app/test.js
+++ app/test.js
@@ -1,20 +1,8 @@
 'use strict';
 import Vue from 'vue';
 import axiosBase from './AxiosBase';
-// import resetScss from './scss/_reset.scss';
-// import bootstrapVue from 'bootstrap-vue';
-import './scss/_reset.scss';
-// import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false;
-// Vue.use(bootstrapVue);
-
-// Vue.mixin(resetScss);
-Vue.mixin(axiosBase);
-
-Vue.component('test', require('./components/test').default);
-
 const EnpReistCar = new Vue({
-  el: '#wrapper'
+  el: '#wrapper',
 });
