b1a8408f81e6f084168141a721454df037a3b7ab
kozo804 <fazsegtwin@gmail.com>
2020-12-04 12:27:59 +0900

    add

---
+++ app/emp_car_regist.js
@@ -0,0 +1,12 @@
+'use strict';
+import Vue from 'vue';
+import axiosBase from './AxiosBase';
+
+Vue.config.productionTip = false;
+
+const EnpReistCar = new Vue({
+  el: '#wrapper',
+  data: {
+    values: ''
+  }
+});
