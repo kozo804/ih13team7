c0922c74700c6362784407aac1894159fe22d602
kozo804 <fazsegtwin@gmail.com>
2020-12-08 16:27:37 +0900

    ルーター追加

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,31 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login', function(req, res, next) {
 });
+
+router.get('/top', function(req, res, next) {
+  
+});
+
+router.get('/car', function(req, res, next) {
+  
+});
+
+router.get('/car/:car_id', function(req, res, next) {
+  
+});
+
+router.get('/auction', function(req, res, next) {
+  
+});
+
+router.get('/auction/:auction_id', function(req, res, next) {
+  
+});
+
+router.get('/auction/:auction_id/bit', function(req, res, next) {
+  
+})
 
 module.exports = router;
