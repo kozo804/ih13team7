e7511699e0e534c79169dee66bc474565cd7e153
gimletus <gimlet.koban@gmail.com>
2020-12-26 00:26:47 +0900

    new file & edit

--- routes/emp.js
+++ routes/emp.js
@@ -179,7 +179,11 @@ router.post('/car/finish', function (req, res, next) {
 });
 
 router.get('/auction', (req,res,next)=>{
+  // DBからオークション履歴と今後のスケジュール取得
+
+
   res.render('emp_auction.ejs');
+  // res.render('emp_auction', data);
 });
 
 router.get('/auction/regist', (req,res,next)=>{
