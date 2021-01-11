ebec4c67312c884fb281a447afb697f91393db01
chenrongchao <chenmiaochaonp@gmail.com>
2021-01-11 13:33:08 +0900

    emp_car : cars display being grid wihtout img(temp use catAPI). About left, do it after

--- app/emp_car.js
+++ app/emp_car.js
@@ -1,20 +1,27 @@
 'use strict';
 import Vue from 'vue';
 import axiosBase from './AxiosBase';
-
+import axios from 'axios'
 Vue.config.productionTip = false;
 
 const EmpCar = new Vue({
   el: '#wrapper',
   data: {
-    cars:""
+    cars:"",
+    imgSrc:""
   },
   methods:{
     
   },
   mounted () {
+    axios.get('https://api.thecatapi.com/v1/images/search?limt=1').then(res => {
+      console.log(res.data[0].url)
+      this.imgSrc = res.data[0].url
+      return res.data[0].url
+    })
     console.log("aaaaa")
-    console.log(this.$refs.cars)
+    this.cars = JSON.parse(this.$refs.cars.value)
+    console.log(this.cars)
   }
 });
 
