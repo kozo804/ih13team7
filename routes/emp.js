f5ddd5217eab985d21cb1350850b10905c5b7bc1
gimletus <gimlet.koban@gmail.com>
2021-01-17 21:40:57 +0900

    edit emp.js /auction databaseからcar一覧を取得、emp_auctionにrender

--- routes/emp.js
+++ routes/emp.js
@@ -186,10 +186,13 @@ router.post('/car/finish', function (req, res, next) {
 
 router.get('/auction', (req,res,next)=>{
   // DBからオークション履歴と今後のスケジュール取得
+  const data = carModel.find(function (err, result){
+    if (err) return console.log(err);
+    console.log(result);
+  })
 
-
-  res.render('emp_auction.ejs');
-  // res.render('emp_auction', data);
+  // res.render('emp_auction.ejs');
+  res.render('emp_auction', {auction: data});
 });
 
 router.get('/auction/regist', async (req,res,next)=>{
