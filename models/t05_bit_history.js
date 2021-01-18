8d3e3bfd5ec078c1193473ebfcef8554857b226b
kozo804 <fazsegtwin@gmail.com>
2021-01-18 19:07:52 +0900

    一旦push

--- models/t05_bit_history.js
+++ models/t05_bit_history.js
@@ -9,7 +9,8 @@ var bitSchema = new Schema({
 	car_id: String,
 	user_id: String, 
 	user_name: String,
-	price: String
+	price: String,
+	date: String
 });
 
 exports.bitHistory = mongoose.model('bitHistory', bitSchema);
